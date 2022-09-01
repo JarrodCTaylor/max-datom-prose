b551b9d577c5c7942471e9f6d8b3cfdd45e2e563
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-01 11:14:23 -0300

    Update index.clj

--- index.clj
+++ index.clj
@@ -2,10 +2,10 @@
                   :slugline "An Interactive Datomic™ Learning Experience"
                   :btn-start "Start"
                   :btn-continue "Continue"}
-        :portuguese {:subtitle ""
-                     :slugline ""
-                     :btn-start ""
-                     :btn-continue ""}
+        :portuguese {:subtitle "Assistente Pessoal\u00A0 Análogo"
+                     :slugline "Uma Experiência Interativa de Aprendizado de Datomic™"
+                     :btn-start "Começar"
+                     :btn-continue "Continuar"}
         :spanish {:subtitle ""
                   :slugline ""
                   :btn-start ""
