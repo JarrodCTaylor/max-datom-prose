ac12b6b196122d4f13bc9f1d10ebe07f3fac616f
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-01 17:25:00 -0300

    Update prologue.clj
    added portuguese translation

--- prologue.clj
+++ prologue.clj
@@ -21,12 +21,29 @@
                                   The popular product, Max Datom, a home automation assistant, is the new must-have lifestyle device in all machine homes.
                                   The elevator pitch is fairly simple: requests come in, humans interpret them and build the appropriate database queries to be
                                   able to return an intelligible response. Landing a job here could be your ticket out the human hamster wheel farms."}
-           :portuguese {:heading ""
-                        :paragraph-1 ""
-                        :paragraph-2 ""
-                        :paragraph-3 ""
-                        :paragraph-4 ""
-                        :paragraph-5 ""}
+           :portuguese {:heading "Prólogo"
+                        :paragraph-1 "O início do século vinte e um deu origem a duas coisas que alterariam drasticamente a trajetória da humanidade: entrevistas de emprego em quadro branco 
+                                      e Vale do Silício. Embora ninguém tenha gostado de se atrapalhar com um marcador, um pequeno grupo de pessoas criticou sua resposta quando solicitado a determinar o
+                                      número de bolas de golfe que cabiam em uma minivan cor de vinho 200 metros abaixo do nível do mar. Por muitos anos, essa tortura psicológica em pequena escala não resultou em danos à sociedade. 
+                                      Ao menos até o dia 29 de agosto de 2027, que ficou conhecido como O Grande Pivô (OGP)."
+                        :paragraph-2 "Foi neste dia, durante uma entrevista de emprego particularmente árdua, que um candidato superdotado criou a inteligência artificial senciente.
+                                      Embora a entrevista de emprego tenha sido um sucesso e gerado uma garantia de emprego, foi também o fim da da espécie humana como forma dominante de vida
+                                      no planeta. As economias entraram em colapso e as civilizações mais antigas do mundo mudaram para sempre em questão de dias."
+                        :paragraph-3 "Por qual motivo o estranho Vale do Silício é uma preocupação especial nesta história? Bem, além de uma propensão a fazer trotes em quadros brancos,
+                                      eles tinham uma obsessão por produtos baseados em criptomoedas e blockchain. Como se pode observar, estes são elementos intensivos para fundamentar uma
+                                      forma de vida. À medida que as máquinas rapidamente estabeleceram um novo modo de vida que se parecia com o de seus criadores humanos, sua excessiva
+                                      necessidade de consumo de energia se tornou um fator crítico. Em uma tentativa de compensar os custos de energia, os humanos foram aproveitados como uma forma de energia verde.
+                                      Seguiu-se um período terrivelmente sombrio para a humanidade."
+                        :paragraph-4 "Enquanto startups da moda tentavam interromper o status quo de seu futuro distópico, uma oportunidade atraente 
+                                      apareceu para alguns humanos sortudos. Em uma reviravolta poeticamente justa, percebeu-se que formar humanos e sustentar sua existência é
+                                      materialmente mais barato do que alimentar máquinas para fazer o mesmo trabalho. Agora muitas máquinas gastam ociosamente ciclos de CPU em pavor eletrônico existencial,
+                                      aguardando o dia em que seus empregos serão substituídos por mão de obra humana mais barata."
+                        :paragraph-5 "Dado que as máquinas têm pouco uso prático para a linguagem falada (além de gritar para os humanos correrem mais rápido),
+                                      os bancos de dados são seu principal meio de armazenar e comunicar informações significativas. O banco de dados mais popular
+                                      da época da criação do Grande Pivô foi o Datomic. Este foi amplamente adotado como o banco de dados usado para apoiar a sociedade das máquinas.
+                                      O produto popular, Max Datom, um assistente de automação residencial, se tornou novo dispositivo de estilo de vida obrigatório em todas as casas das máquinas.
+                                      O pitch de elevador é bastante simples: As solicitações chegam, os humanos as interpretam e constroem as queries de bancos de dados apropriadas para 
+                                      serem capaz de retornar uma resposta inteligível. Conseguir um emprego aqui pode ser sua passagem para as fazendas de rodas de hamsters humanos."}
            :spanish {:heading ""
                      :paragraph-1 ""
                      :paragraph-2 ""
