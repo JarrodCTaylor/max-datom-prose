882d7f31d31240e41c37a06114f924a90aae4388
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-01 14:40:53 -0300

    Update level-1.clj
    added portuguese translation

--- level-1.clj
+++ level-1.clj
@@ -10,15 +10,17 @@
                     :paragraph-5.2 ":book/author"
                     :paragraph-5.3 ", instead of :book/publication-date."
                     :paragraph-6 (str "Congratulations, human " applicant-name "! You are officially the newest member of the Max Datom training cohort " (random-uuid) ". Please continue to the training area.")}
-          :portuguese {:level-title ""
-                       :paragraph-1 (str "" applicant-name "")
-                       :paragraph-3.1 ""
-                       :paragraph-3.2 ""
-                       :paragraph-4 ""
-                       :paragraph-5.1 ""
+          :portuguese {:level-title "Triagem" 
+                       :paragraph-1 (str "Seja bem-vindo, humano, " applicant-name". Este é o Campus de Treinamento Max Datom/
+                                                                                     Primeiro, você vai ser submetido a uma triagem básica de aptidão. Se você for bem-sucedido nesta etapa, você pode entrar oficialmente no programa de treinamento.
+                                                                                     Se você não for, bem... Eu tenho certeza que você será bem sucedido. Você parece muito mais capaz que o último aplicante que tivemos aqui.")
+                       :paragraph-3.1 "Comece rodando a query que disponibilizamos pra você na seção de entrada que está à direita."
+                       :paragraph-3.2 "Esta query vai retornar todas as datas de publicação para a entidade de livros (book entities)."
+                       :paragraph-4 "Parabéns! Você apertou um botão com sucesso. *Nota: a habilidade cognitiva do candidato não aparenta ter sido severamente danificada por esforços anteriores que requeriram energia."
+                       :paragraph-5.1 "Agora, modifique a query para retornar os valures na database para"
                        :paragraph-5.2 ":book/author"
-                       :paragraph-5.3 ", instead of :book/publication-date." ;; `:book/publication-date needs to stay untranslated`
-                       :paragraph-6 (str "" applicant-name "" (random-uuid) "")}
+                       :paragraph-5.3 ", ao invés de :book/publication-date." ;; 
+                       :paragraph-6 (str "Parabéns, humano "applicant-name"! Você é oficialmente o mais novo membro do grupo de treinamento Max Datom "(random-uuid)". Por favor, siga para área de treinamento.")}
           :spanish {:level-title ""
                     :paragraph-1 (str "" applicant-name "")
                     :paragraph-3.1 ""
