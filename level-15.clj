7d76349632b9d6698d744988b9973e3a0f0ab206
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-01 16:28:44 -0300

    Update level-15.clj
    added portuguese translation

--- level-15.clj
+++ level-15.clj
@@ -4,11 +4,12 @@
                      :paragraph-2 "Pull all :transfer/from and :transfer/to data including :account/owner for a recently reported transfer "
                      :paragraph-2.1 ":transfer/id #uuid \"59B9C791-74CE-4C51-A4BC-EF6D06BEE2DBA\""
                      :paragraph-3 "That transfer looks like we would expect, but why did the funds not make it to Mr. CD? Continue to level 16 to investigate further."}
-           :portuguese {:level-title ""
-                        :paragraph-1 (str "" applicant-name "")
-                        :paragraph-2 ":transfer/from :transfer/to :account/owner"
+           :portuguese {:level-title "Nível 15"
+                        :paragraph-1 (str "Humano " applicant-name ", nós temos razões para acreditar que a Silicone Crime Family está fazendo uma transferência corrupta de moeda crypto. Há inúmeros relatórios
+                                                                      de requisições de transferência quais o dinheiro é removido de uma conta mas nunca chega ao destinatário pretendido. Nós precisamos reunir evidências concretas de atividade criminal.")
+                        :paragraph-2 "Puxe todos os dados de :transfer/from e :transfer/to incluindo :account/owner para uma transferência informada recentemente "
                         :paragraph-2.1 ":transfer/id #uuid \"59B9C791-74CE-4C51-A4BC-EF6D06BEE2DBA\""
-                        :paragraph-3 ""}
+                        :paragraph-3 "Essa transferência parece normal, mas por que os fundos não chegaram ao Sr. CD? Continue até o nível 16 para investigar mais."}
            :spanish {:level-title ""
                      :paragraph-1 (str "" applicant-name "")
                      :paragraph-2 ":transfer/from :transfer/to :account/owner"
