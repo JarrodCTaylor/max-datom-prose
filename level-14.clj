ec0f8625fe4bf911988f12237e026a31ab939e51
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-01 15:59:31 -0300

    Update level-14.clj
    added portuguese translation

--- level-14.clj
+++ level-14.clj
@@ -15,14 +15,23 @@
                                   and would force the query engine to consider every worker in the database before narrowing down to a specific worker-id. Putting the most selective clause first can cause dramatic improvements in performance."
                      :paragraph-6 "Reorder the clauses in the query to the right so that the more selective clauses come first."
                      :paragraph-7 "You may now continue to level 15."}
-           :portuguese {:level-title ""
-                        :paragraph-1 (str "" applicant-name "" applicant-name "")
-                        :paragraph-2 ""
-                        :paragraph-3 ""
+           :portuguese {:level-title "Nível 14"
+                        :paragraph-1 (str "Humano " applicant-name ", entre e pegue uma cadeira. Você se provou ser um dos melhores associados do escritório Max Datom (TM).
+                                          É hora de explicar o verdadeiro propósito do Max Datom. Humanos não são observadores por natureza: isso misturado com uma baixa capacidade intelectual 
+                                          os faz menos interessantes e não os torna uma ameaça para máquinas. Humano " applicant-name ", por favor preste atenção. Isso é importante! Como eu estava dizendo, o cômico nível de inferioridade
+                                                                                                                                                                    exibido por humanos é precisamento o que os faz os agentes secretos perfeitos para os usar em um esforço para derrubar a Silicone Crime Family. O Departamento de Investigação Não-Fungível
+                                          vem trabalhando há anos para conseguir uma condenação. A configuração do Max Datom nos permitiu persuadir os membros da família a baixar a guarda e permitir o acesso humano aos dados.
+                                                                                                                                                            Nós estivemos esperando um de vocês se provar competente o suficiente para passar esta tarefa. Eu gostaria de dizer que nós achamos que você tem o necessário para isso; mas isso é mentira que estou programado pelo bureau 
+                                          para não contar. O fato é que estamos sem tempo e você demonstrou um nível mínimo absoluto de habilidade para possivelmente ter uma chance. Este trabalho vem com um grande risco pessoal.
+                                                                                                                                                            Dadas as suas opções alternativas, vou assumir que contamos com sua total cooperação.")
+                        :paragraph-2 "Precisamos acelerar o ritmo para prepará-lo. O desempenho da query é de extrema importância. As cláusulas :where das queries do Datomic são executadas em ordem.
+                                      Para minimizar o trabalho realizado pelo mecanismo de queries, as cláusulas mais restritivas devem vir antes das cláusulas menos restritivas."
+                        :paragraph-3 "Por exemplo:"
                         :paragraph-4 "(d/q '[:find ?farm-name\n       :in $ ?worker-id\n       :where [?worker :worker/id ?worker-id]\n              [?farm :farm/workers ?worker]\n              [:?farm :farm/name ?farm-name]] db \"123-abc\")"
-                        :paragraph-5 ":worker/id :farm/workers"
-                        :paragraph-6 ""
-                        :paragraph-7 ""}
+                        :paragraph-5 "Observe como a cláusula mais específica :worker/id é a primeira. Se a query tivesse :farm/workers primeiro, isso não seria seletivo,
+                                      e forçaria a query a considerar qualquer trabalhador na base de dados antes de restringir a um ID de trabalhador específico. Colocar a cláusula mais seletiva em primeiro lugar pode causar melhorias dramáticas no desempenho."
+                        :paragraph-6 "Reordene as cláusulas na query à direita para que as cláusulas mais seletivas venham primeiro."
+                        :paragraph-7 "Você pode seguir para o nível 15."}
            :spanish {:level-title ""
                      :paragraph-1 (str "" applicant-name "" applicant-name "")
                      :paragraph-2 ""
