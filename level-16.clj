452c82770c8a438b8dcb4d0dc32b1617689d770f
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-01 16:34:42 -0300

    Update level-16.clj
    added portuguese translation

--- level-16.clj
+++ level-16.clj
@@ -9,16 +9,17 @@
                      :paragraph-5 "Use the history db to query e, v, t and op for the :transfer/to attribute associated with "
                      :paragraph-5.1 ":transfer/id #uuid \"59B9C791-74CE-4C51-A4BC-EF6D06BEE2DBA\""
                      :paragraph-6 " This is interesting, it appears as though the value was added and then later retracted and replaced with a different value. You may now continue to level 17."}
-           :portuguese {:level-title ""
-                        :paragraph-1 (str "" applicant-name "")
+           :portuguese {:level-title "Nível 16"
+                        :paragraph-1 (str "Humano" applicant-name ", a transferência pode parecer normal agora, mas há uma razão pela qual os fundos não chegaram ao 
+                                                                    destinatário pretendido. Ao alavancar a uma")
                         :paragraph-1.1 "history database" ;; This is linked to the docs about history databases
-                        :paragraph-1.2 ""
-                        :paragraph-2 ""
+                        :paragraph-1.2 "você pode fazer uma query para consultar todas as afirmações e retratações independentemente de quando elas ocorreram."
+                        :paragraph-2 "Por exemplo"
                         :paragraph-3 "(d/q '[:find ?e ?v ?tx ?op \n       :in $ ?worker-id\n       :where [?e :worker/id ?worker-id]\n              [?e :worker/rating ?v ?tx ?op]] (d/history db) \"123-abc\")"
-                        :paragraph-4 ":work/rating :worker/id \"123-abc\""
-                        :paragraph-5 "e, v, t op :transfer/to "
+                        :paragraph-4 "A consulta acima retornará todas as entidades, valores, transações e operações para quaisquer afirmações ou retrações para o atributo :work/rating da entidade associada a :worker/id \"123-abc\""
+                        :paragraph-5 "Use o histórico db para fazer uma query e consultar e, v, t e op para o atributo :transfer/to associado com "
                         :paragraph-5.1 ":transfer/id #uuid \"59B9C791-74CE-4C51-A4BC-EF6D06BEE2DBA\""
-                        :paragraph-6 ""}
+                        :paragraph-6 "Isso é interessante, parece que o valor foi adicionado e depois retraído e substituído por um valor diferente. Agora você pode continuar até o nível 17."}
            :spanish {:level-title ""
                      :paragraph-1 (str "" applicant-name "")
                      :paragraph-1.1 "history database" ;; This is linked to the docs about history databases
