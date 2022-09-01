349cde0d794e4d08c4e521b3988337f080c35632
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-01 15:08:28 -0300

    Update level-10.clj
    added portuguese translation

--- level-10.clj
+++ level-10.clj
@@ -15,20 +15,23 @@
                      :paragraph-5.4 " for the user associated with "
                      :paragraph-5.5 ":user/id #uuid \"1B341635-BE22-4ACC-AE5B-D81D8B1B7678\""
                      :paragraph-6 "You may now continue to level 11."}
-           :portuguese {:level-title ""
-                        :paragraph-1 (str "" applicant-name "")
-                        :paragraph-2 ""
-                        :paragraph-2.1 ""
-                        :paragraph-2.2 ""
-                        :paragraph-3 ""
+           :portuguese {:level-title "Nível 10"
+                        :paragraph-1 (str "Parabéns, humano " applicant-name "! Você completou o programa de entrada para novos associados e agora é oficialmente um membro júnior do time Max Datom (TM). 
+                                                                                Eu tenho certeza que seus pais ficariam orgulhosos. Eu enviaria a eles a notícia da sua conquista se todas as comunicações de entrada e saída
+                                                                                de fazendas de trabalho não fossem estritamente proibidas. Para comemorar seu novo status eu estou presenteando você com sua própria camiseta polo do Max Datom (TM). Nós entendemos
+                                                                                que por alguma razão, camisetas polo fazem humanos se sentirem parte de um time. A camiseta é sua. O custo dela vai ser deduzido de seus futuros créditos ganhos.")
+                        :paragraph-2 "Chega de comemorações. Você ainda tem muito a aprender. Uma"
+                        :paragraph-2.1 "aggregation function (função agregada)"
+                        :paragraph-2.2 "aparece em uma cláusula de busca e transforma um resultado. Funções agregadas podem receber variáveis, constantes ou src-vars como argumentos."
+                        :paragraph-3 "Por exemplo, para retornar uma contagem de trabalhadores atribuídos a cada fazenda de energia junto com o nome da fazenda, a seguinte consulta poderia ser feita;"
                         :paragraph-4 "(d/q '[:find (count ?workers) ?farm-name\n       :where [?e :farm/name ?farm-name]\n              [:?e :farm/workers ?workers]] db)"
-                        :paragraph-5 ""
+                        :paragraph-5 "Depois de inspecionar o novo schema do conjunto de dados, atualize a consulta para retornar o"
                         :paragraph-5.1 ":user/first+last-name"
-                        :paragraph-5.2 ""
+                        :paragraph-5.2 "e contagem de"
                         :paragraph-5.3 ":post/author"
-                        :paragraph-5.4 ""
+                        :paragraph-5.4 "para o usuário associado com"
                         :paragraph-5.5 ":user/id #uuid \"1B341635-BE22-4ACC-AE5B-D81D8B1B7678\""
-                        :paragraph-6 ""}
+                        :paragraph-6 "Você pode agora continuar para o nível 11."}
            :spanish {:level-title ""
                      :paragraph-1 (str "" applicant-name "")
                      :paragraph-2 ""
