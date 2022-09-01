cea4ea08e5013ad6f58ee5873a3b712b5da282fa
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-01 14:43:52 -0300

    Update modal.clj
    added portuguese translation

--- modal.clj
+++ modal.clj
@@ -1,9 +1,9 @@
 :modal {:english {:title "Options"
                   :level "Level Select"
                   :language "Language Select"}
-        :portuguese {:title ""
-                     :level ""
-                     :language ""}
+        :portuguese {:title "Opções"
+                     :level "Selecione o Nível"
+                     :language "Selecione a Língua"}
         :spanish {:title ""
                   :level ""
                   :language ""}}
