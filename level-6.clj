34aa4bec374610c93ca7563d2c2cb4d497a53eee
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-02 10:14:19 -0300

    Update level-6.clj
    added portuguese translation

--- level-6.clj
+++ level-6.clj
@@ -8,16 +8,16 @@
                     :paragraph-4.1 ":book/author "
                     :paragraph-4.2 "using the underscore prefix syntax."
                     :paragraph-5 "You may now continue to level 7."}
-          :portuguese {:level-title ""
-                       :paragraph-1 (str "" applicant-name "")
-                       :paragraph-1.1 ""
-                       :paragraph-1.2 ""
+          :portuguese {:level-title "Nível 6"
+                       :paragraph-1 (str "Humano " applicant-name "normalmente, pull retorna um mapa de atributos e valores selecionados de uma entidade específica.")
+                       :paragraph-1.1 "Um prefixo sublinhado"
+                       :paragraph-1.2 "/_ no componente de nome local de um atributo faz com que o atributo seja navegado em reverso. Por exemplo:"
                        :paragraph-2 "[:find (pull ?e [:farm/_workers])\n :where [?e :worker/id #uuid \"DC122EEA-5D92-45CE-98ED-874AFA648CEE\"]"
-                       :paragraph-3 ":farm/workers [:worker/id #uuid \"DC122EEA-5D92-45CE-98ED-874AFA648CEE\"]"
-                       :paragraph-4 ""
+                       :paragraph-3 "puxará quaisquer entidades que tenham um atributo :farm/workers com o valor de [:worker/id #uuid \"DC122EEA-5D92-45CE-98ED-874AFA648CEE\"]"
+                       :paragraph-4 "Modifique a query para retornar quaisquer entidades que façam referência ao id na query como um valor para"
                        :paragraph-4.1 ":book/author "
-                       :paragraph-4.2 ""
-                       :paragraph-5 ""}
+                       :paragraph-4.2 "usando a sintaxe do prefixo de sublinhado."
+                       :paragraph-5 "Você pode seguir para o nível 7."}
           :spanish {:level-title ""
                     :paragraph-1 (str "" applicant-name "")
                     :paragraph-1.1 ""
