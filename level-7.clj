ed842e2fead5946f3d7b5dbf7434f6a5e286df1b
gpalharini <99666145+gpalharini@users.noreply.github.com>
2022-09-02 10:19:55 -0300

    Update level-7.clj
    added portuguese translation

--- level-7.clj
+++ level-7.clj
@@ -9,16 +9,17 @@
                     :paragraph-2.3 ":book/publication-date "
                     :paragraph-2.4 "instead of the :db/id of the book entities."
                     :paragraph-3 "You may now continue to level 8."}
-          :portuguese {:level-title ""
-                       :paragraph-1 (str "" applicant-name ":db/id :author/id")
+          :portuguese {:level-title "Nível 7"
+                       :paragraph-1 (str "Humano " applicant-name ", atualmente, a query retorna o :db/id para as entidades do livro que fazem referência ao :author/id
+                                         da entidade autor que está sendo consultada. Pull ajuda a fazer seleções aninhadas de informações sobre entidades. Usando a sintaxe ")
                        :paragraph-1.1 "{:parent/attr [:nested-attr1 :nested-attr2]}"
-                       :paragraph-1.2 " :nested-attr1  :nested-attr2 :db/id :parent/attr"
-                       :paragraph-2 ""
+                       :paragraph-1.2 "como um padrão na expressão pull, serão retornados os valores de :nested-attr1 e :nested-attr2 ao invés de :db/id para :parent/attr"
+                       :paragraph-2 "Modifique a query para retornar os valores para"
                        :paragraph-2.1 ":book/name "
-                       :paragraph-2.2 ""
+                       :paragraph-2.2 "e"
                        :paragraph-2.3 ":book/publication-date "
-                       :paragraph-2.4 ":db/id "
-                       :paragraph-3 ""}
+                       :paragraph-2.4 "ao invés do :db/id das entidades do livro."
+                       :paragraph-3 "Você pode seguir para o nível 8."}
           :spanish {:level-title ""
                     :paragraph-1 (str "" applicant-name ":db/id :author/id")
                     :paragraph-1.1 "{:parent/attr [:nested-attr1 :nested-attr2]}"
