4a181aaa9daef492e4b9b61fa8667c6685d167d0
Bere Beltran <71998716+beretech@users.noreply.github.com>
2022-09-06 15:42:01 -0500

    Spanish translation

--- level-13.clj
+++ level-13.clj
@@ -30,19 +30,19 @@
                         :paragraph-5.3 "{:post/stats (str \"Likes: \" <<postLikeCount>> \" Dislikes: \" <<postDislikeCount>>)}"
                         :paragraph-5.4 "para cada postagem."
                         :paragraph-6 "Você pode seguir para o nível 14."}
-           :spanish {:level-title ""
-                     :paragraph-1 (str "" applicant-name "")
+           :spanish {:level-title "Nivel 14"
+                     :paragraph-1 (str "Humano " applicant-name ", puedes escribir tu propia ")
                      :paragraph-1.1 "custom functions"
-                     :paragraph-1.2 ""
-                     :paragraph-2 ""
+                     :paragraph-1.2 " para usar como aggregate function, de predicado o cláusula de función en queries."
+                     :paragraph-2 "Por ejemplo, para regresar "
                      :paragraph-2.1 "{:stats {:worker/age 27 :days-since-disciplinary-incident 2}"
-                     :paragraph-2.2 ""
+                     :paragraph-2.2 " en lugar de la ref en "
                      :paragraph-2.3 ":farm/top-worker"
-                     :paragraph-2.4 ""
+                     :paragraph-2.4 ", se puede aprovechar una custom function."
                      :paragraph-3 "(defn worker-stats [db worker-eid]\n  (let [{:worker/keys [age days-since-disciplinary-incident]} (d/pull db '[:worker/age :worker/days-since-disciplinary-incident] worker-eid)]\n        {:worker/stats {:age age :days-since-disciplinary-incident days-since-disciplinary-incident}}))\n\n(d/q '[:find (pull ?farm [:farm/name]) ?worker-stats\n       :where [?farm :farm/top-worker ?top-worker]\n              [(datomic-scratch/worker-stats $ ?top-worker) ?worker-stats]] db)"
-                     :paragraph-5 ""
+                     :paragraph-5 "Usando una custom function, actualiza el query para regresar el "
                      :paragraph-5.1 ":user/first+last-name"
-                     :paragraph-5.2 ""
+                     :paragraph-5.2 " del autor y "
                      :paragraph-5.3 "{:post/stats (str \"Likes: \" <<postLikeCount>> \" Dislikes: \" <<postDislikeCount>>)}"
-                     :paragraph-5.4 ""
-                     :paragraph-6 ""}}
+                     :paragraph-5.4 " de cada post."
+                     :paragraph-6 "Ahora puedes continuar al nivel 14."}}
