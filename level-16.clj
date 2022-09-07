e7b7783e9191fd9b019b835b8f676f2c2ffac54c
Bere Beltran <71998716+beretech@users.noreply.github.com>
2022-09-07 13:06:29 -0500

    Spanish translation

--- level-16.clj
+++ level-16.clj
@@ -1,5 +1,5 @@
 :level-16 {:english {:level-title "Level 16"
-                     :paragraph-1 (str "Human " applicant-name ", the transfer may appear normal now but there has to be a reason the funds didn't make it to the
+                     :paragraph-1 (str "Human " applicant-name ", the transfer may appear normal now, but there has to be a reason the funds didn't make it to the
                                                                intended destination. By leveraging a ")
                      :paragraph-1.1 "history database"
                      :paragraph-1.2 " you can query all assertions and retractions regardless of when they occurred."
@@ -21,12 +21,13 @@
                         :paragraph-5.1 ":transfer/id #uuid \"59B9C791-74CE-4C51-A4BC-EF6D06BEE2DBA\""
                         :paragraph-6 "Isso é interessante, parece que o valor foi adicionado e depois retirado e substituído por um valor diferente. Você pode seguir para o nível 17."}
            :spanish {:level-title ""
-                     :paragraph-1 (str "" applicant-name "")
+                     :paragraph-1 (str "Humano " applicant-name ", la transferencia puede parecer normal ahora, pero tiene que haber una razón por la cual los fondos no llegaron al
+                                                                destino propuesto. Al usar una ")
                      :paragraph-1.1 "history database" ;; This is linked to the docs about history databases
-                     :paragraph-1.2 ""
-                     :paragraph-2 ""
+                     :paragraph-1.2 "puedes consultar todas las afirmaciones y retractaciones independientemente de cuándo ocurrieron."
+                     :paragraph-2 "Por ejemplo:"
                      :paragraph-3 "(d/q '[:find ?e ?v ?tx ?op \n       :in $ ?worker-id\n       :where [?e :worker/id ?worker-id]\n              [?e :worker/rating ?v ?tx ?op]] (d/history db) \"123-abc\")"
-                     :paragraph-4 ":work/rating :worker/id \"123-abc\""
-                     :paragraph-5 "e, v, t op :transfer/to "
+                     :paragraph-4 "El query anterior regresa todas las entidades, valores, transacciones y operaciones para cualquier aserción o retractación del atributo :work/rating de la entidad asociada con :worker/id \"123-abc\""
+                     :paragraph-5 "Usa el history database para consultar e, v, t y op para el atributo :transfer/to asociado con "
                      :paragraph-5.1 ":transfer/id #uuid \"59B9C791-74CE-4C51-A4BC-EF6D06BEE2DBA\""
-                     :paragraph-6 ""}}
+                     :paragraph-6 "Esto es interesante, parece que el valor se agregó y luego se retractó y se reemplazó con un valor diferente. Ahora puedes continuar al nivel 17."}}
