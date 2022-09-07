6249318e3e3dcbfbb270fb70558c9bc16525e333
Bere Beltran <71998716+beretech@users.noreply.github.com>
2022-09-07 12:34:49 -0500

    Spanish translation

--- level-15.clj
+++ level-15.clj
@@ -10,8 +10,9 @@
                         :paragraph-2 "Puxe todos os dados de :transfer/from e :transfer/to incluindo :account/owner para uma transação reportada recentemente "
                         :paragraph-2.1 ":transfer/id #uuid \"59B9C791-74CE-4C51-A4BC-EF6D06BEE2DBA\""
                         :paragraph-3 "Essa transferência parece normal, mas por que os fundos não chegaram ao Sr. CD? Continue até o nível 16 para investigar mais."}
-           :spanish {:level-title ""
-                     :paragraph-1 (str "" applicant-name "")
-                     :paragraph-2 ":transfer/from :transfer/to :account/owner"
+           :spanish {:level-title "Nivel 15"
+                     :paragraph-1 (str "Humano " applicant-name ", tenemos razones para creer que la Silicone Crime Family está ejecutando un intercambio corrupto de criptomonedas. Ha habido numerosos reportes
+                                                                de solicitudes de transferencia en las que el dinero se retira de una cuenta pero nunca llega al destinatario previsto. Necesitamos obtener pruebas concretas de actividad delictiva.")
+                     :paragraph-2 "Obtén todos los datos de :transfer/from y :transfer/to incluyendo :account/owner para la transferencia reportada recientemente "
                      :paragraph-2.1 ":transfer/id #uuid \"59B9C791-74CE-4C51-A4BC-EF6D06BEE2DBA\""
-                     :paragraph-3 ""}}
+                     :paragraph-3 "Esa transferencia se ve como esperaríamos, pero ¿por qué los fondos no llegaron al Sr. CD? Continúa al nivel 16 para investigar más a fondo."}}
